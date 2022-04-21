b1837d8e1c4a3eb8ac38555f1c093544e6b331c6
kamontander <matej.korlaet@gmail.com>
2022-04-21 11:18:58 +0200

    first commit

---
+++ js/questions.js
@@ -0,0 +1,103 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "Razgradnjom proteina u probavnom sustavu stvaraju se važne gradivne komponente svih naših stanica, a to su:",
+      answer: "aminokiseline",
+      options: [
+        "aminokiseline",
+        "vitamini",
+        "kolagen"
+      ]
+    },
+    {
+      numb: 2,
+      question: "Za bržu izgradnju mišića, osim rada u teretani (ili oko nje) trebaš jesti:",
+      answer: "hranu bogatu proteinima",
+      options: [
+        "hranu bogatu proteinima",
+        "špinat kao Popaj",
+        "voće"
+      ]
+    },
+    {
+      numb: 3,
+      question: "Što je quark?",
+      answer: "svježi sir s visokim udjelom proteina i niskim udjelom mliječne masti i ugljikohidrata",
+      options: [
+        "vrsta goriva",
+        "svježi sir s visokim udjelom proteina i niskim udjelom mliječne masti i ugljikohidrata",
+        "vrsta jogurta"
+      ]
+    },
+    {
+      numb: 4,
+      question: "Ako jedeš hranu bogatu proteinima i prehrana ti se bazira na većinskom unosu proteina, važno je prehrani dodati:",
+      answer: "vlakna",
+      options: [
+        "kalcij",
+        "kalij",
+        "vlakna"
+      ]
+    },
+    {
+      numb: 5,
+      question: "Točno ili netočno: moraš jesti isključivo meso da bi unosio dovoljno proteina?",
+      answer: "netočno",
+      options: [
+        "točno",
+        "netočno",
+        "nemam pojma"
+      ]
+    },
+    {
+      numb: 6,
+      question: "Skyr, posebna vrsta jogurta s Islanda, poznata je po tome što:",
+      answer: "ima puno proteina i malo masti",
+      options: [
+        "ima okus po jagodama",
+        "ima puno proteina i malo masti",
+        "ima čudno ime"
+      ]
+    },
+    {
+      numb: 7,
+      question: "Ako vježbaš ili treniraš i radiš na mišićnoj masi, ključno vrijeme za uzimanje proteina je:",
+      answer: "do 30 minuta poslije treninga",
+      options: [
+        "do 30 minuta poslije treninga",
+        "prije ručka",
+        "za volanom"
+      ]
+    },
+    {
+      numb: 8,
+      question: "Što je Lactobacillus acidophilus?",
+      answer: "dobre bakterije koje se nalazi u jogurtu",
+      options: [
+        "dio automobila spojen s cinculatorom",
+        "dobre bakterije koje se nalazi u jogurtu",
+        "latinski izraz za pobjednika rallyja"
+      ]
+    },
+    {
+      numb: 9,
+      question: "Vlakna u prehrani ključna su za?",
+      answer: "pravilnu probavu",
+      options: [
+        "pravilnu probavu",
+        "dobre zube",
+        "sprječavanje ćelavosti"
+      ]
+    },
+    {
+      numb: 10,
+      question: "100 grama Posnog sira Active Zone ima samo…",
+      answer: "64 kalorije",
+      options: [
+        "64 kalorije",
+        "320 kalorija",
+        "0 kalorija"
+      ]
+    }
+];
